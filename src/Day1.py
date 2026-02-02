3ca0e94720dc97fc9de424b9c3d7e1727dd3792d
chethan123344 <ckulal014@gmail.com>
2026-02-02 15:09:39 +0530

    Day1: Intial setup and Hello World

---
+++ src/Day1.py
@@ -0,0 +1,8 @@
+Python 3.12.2 (tags/v3.12.2:6abddd9, Feb  6 2024, 21:26:36) [MSC v.1937 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+= RESTART: C:/DS_AI_Internship/new.py
+hello world
+>>> import sys
+>>> print("hello");
+hello
